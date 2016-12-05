88949835749fbe49d86124f6b0ed6e517aab901f
Duane Leem <duaneleem@alumni.ggu.edu>
2016-12-05 00:50:31 -0800

    Mongomart Update.

--- M101JS-mongodb/Exercises Week 8 Final/mongomart/cart.js
+++ M101JS-mongodb/Exercises Week 8 Final/mongomart/cart.js
@@ -1,12 +1,9 @@
 /*
   Copyright (c) 2008 - 2016 MongoDB, Inc. <http://mongodb.com>
-
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,33 +21,44 @@ function CartDAO(database) {
 
     this.db = database;
 
-
+    
     this.getCart = function(userId, callback) {
         "use strict";
-
+        
         /*
         * TODO-lab5
         *
-        * LAB #5: Implement the getCart() method.
-        *
-        * Query the "cart" collection by userId and pass the cart to the
+        * LAB #5: 
+        * Query the "cart" collection by userId and pass the cart to the 
         * callback function.
         *
         */
 
+        /**********************
         var userCart = {
             userId: userId,
             items: []
         }
         var dummyItem = this.createDummyItem();
         userCart.items.push(dummyItem);
-
+        
         // TODO-lab5 Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the userCart to the
-        // callback.
         callback(userCart);
+        **********************/
+
+        this.db.collection('cart')
+            .findOne({ 'userId': userId}, {'_id': 0 ,'items' :1})
+            .toArray(function(err, result) {
+                var userCart = {
+                    userId: userId,
+                    items: result[0].items
+                }
+            console.log(result);
+            //userCart.items.push(result[0]);
+            assert.equal(err, null);
+            console.log(userCart);
+            callback(userCart); 
+        });
     }
 
 
@@ -60,25 +68,25 @@ function CartDAO(database) {
         /*
          *
          * TODO-lab6
-         *
+         * 
          * LAB: #6
          *
-         * Write a query that will determine whether or not the cart associated
-         * with the userId contains an item identified by itemId. If the cart
-         * does contain the item, pass the item to the callback. If it does not,
-         * pass the value null to the callback.
+         * Write a query that will determine whether or not the cart associated 
+         * with the userId contains an item identified by itemId. If the cart 
+         * does contain the item, pass the item to the callback. If it does not, 
+         * pass the value null to the callback. 
          *
-         * NOTE: You should pass only the matching item to the callback. Do not
+         * NOTE: You should pass only the matching item to the callback. Do not 
          * pass an array of one or more items or the entire cart.
          *
-         * SUGGESTION: While it is not necessary, you might find it easier to
-         * use the $ operator in a projection document in your call to find() as
-         * a means of selecting the matching item. Again, take care to pass only
+         * SUGGESTION: While it is not necessary, you might find it easier to 
+         * use the $ operator in a projection document in your call to find() as 
+         * a means of selecting the matching item. Again, take care to pass only 
          * the matching item (not an array) to the callback. See:
          * https://docs.mongodb.org/manual/reference/operator/projection/positional/
          *
-         * As context for this method to better understand its purpose, look at
-         * how cart.itemInCart is used in the mongomart.js app.
+         * As context for this method to better understand its purpose, look at 
+         * how cart.itemInCart is used in the mongomart.js app. 
          *
          */
 
@@ -87,16 +95,16 @@ function CartDAO(database) {
         // TODO-lab6 Replace all code above (in this method).
     }
 
-
+    
     /*
      * This solution is provide as an example to you of several query
-     * language features that are valuable in update operations.
-     * This method adds the item document passed in the item parameter to the
+     * language features that are valuable in update operations. 
+     * This method adds the item document passed in the item parameter to the 
      * user's cart. Note that this solution works regardless of whether the
-     * cart already contains items or is empty. addItem will be called only
+     * cart already contains items or is empty. addItem will be called only 
      * if the cart does not already contain the item. The route handler:
      * router.post("/user/:userId/cart/items/:itemId"...
-     * handles this. Please review how that method works to have a complete
+     * handles this. Please review how that method works to have a complete 
      * understanding of how addItem is used.
      *
      * NOTE: One may use either updateOne() or findOneAndUpdate() to
@@ -135,9 +143,7 @@ function CartDAO(database) {
             });
 
         /*
-
           Without all the comments this code looks written as follows.
-
         this.db.collection("cart").findOneAndUpdate(
             {userId: userId},
             {"$push": {items: item}},
@@ -150,25 +156,25 @@ function CartDAO(database) {
                 callback(result.value);
             });
         */
-
+        
     };
 
 
     this.updateQuantity = function(userId, itemId, quantity, callback) {
         "use strict";
-
+        
         /*
         * TODO-lab7
         *
-        * LAB #7: Update the quantity of an item in the user's cart in the
-        * database by setting quantity to the value passed in the quantity
-        * parameter. If the value passed for quantity is 0, remove the item
+        * LAB #7: Update the quantity of an item in the user's cart in the 
+        * database by setting quantity to the value passed in the quantity 
+        * parameter. If the value passed for quantity is 0, remove the item 
         * from the user's cart stored in the database.
         *
         * Pass the updated user's cart to the callback.
         *
         * NOTE: Use the solution for addItem as a guide to your solution for
-        * this problem. There are several ways to solve this. By far, the
+        * this problem. There are several ways to solve this. By far, the 
         * easiest is to use the $ operator. See:
         * https://docs.mongodb.org/manual/reference/operator/update/positional/
         *
@@ -182,7 +188,7 @@ function CartDAO(database) {
         dummyItem.quantity = quantity;
         userCart.items.push(dummyItem);
         callback(userCart);
-
+        
         // TODO-lab7 Replace all code above (in this method).
 
     }
